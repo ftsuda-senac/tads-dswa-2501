2ac7cdc97045d3f8e4c74b46905638cc77114754
ftsuda-senac <ftsuda.senac@gmail.com>
2025-04-01 19:47:07 -0300

    Versão inicial do projeto dados pessoais

--- exemplo-rest/src/main/java/br/senac/tads/dsw/exemplo/rest/DadosRestController.java
+++ exemplo-rest/src/main/java/br/senac/tads/dsw/exemplo/rest/DadosRestController.java
@@ -1,30 +1,37 @@
 package br.senac.tads.dsw.exemplo.rest;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/dados")
 public class DadosRestController {
 
-    @GetMapping
-    public String findAll() {
-        return "findAll com @RestController";
-    }
+  @Autowired
+  private DadosService service;
 
-    @GetMapping("/{id}")
-    public String findById(@PathVariable("id") Integer id) {
-        return "findById-" + id + "  com @RestController";
-    }
-    
-    
-    @GetMapping("/json")
-    public Dados findJson() {
-        return new Dados("Fulano da Silva", "fulano@email.com");
-    }
+  @GetMapping
+  public List<DadosDto> findAll() {
+    return service.findAll();
+  }
 
+  @GetMapping("/{id}")
+  public DadosDto findById(@PathVariable("id") Integer id) {
+    Optional<DadosDto> optDados = service.findById(id);
+    if (!optDados.isPresent()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    return optDados.get();
+    // OU
+    // return service.findById(id).orElseThrow(
+    //     () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
 }
